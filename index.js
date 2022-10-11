70e8d9db30808b1d2e608e62312fbc4b91d7e183
jwiz <jordanheath945@gmail.com>
2022-10-11 16:09:54 -0400

    added structure to directory to begin with

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const generateHtml = require("./src/generateHtml");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const writeFile = require("./src/generateFiles");
